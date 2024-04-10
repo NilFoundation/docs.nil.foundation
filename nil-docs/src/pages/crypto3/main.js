c571c7e00f8c4b33c33809acc474a2231265f0af
Shukhrat Khannanov <khannanov.sh@gmail.com>
2024-04-10 09:56:30 +0300

    fix to another attempt

--- nil-docs/src/pages/crypto3/main.js
+++ nil-docs/src/pages/crypto3/main.js
@@ -2,7 +2,7 @@ import useBaseUrl from '@docusaurus/useBaseUrl';
 
 export default function Home() {
     React.useEffect(() => {
-        window.location.href = useBaseUrl('/docs/main');
+        window.location.href = useBaseUrl('/crypto3/intro');
     }, []);
     return null;
 }
