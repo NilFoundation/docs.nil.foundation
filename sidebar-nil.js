00dc4a6b4417b808096e50046c316e8b736ea58d
Shukhrat Khannanov <khannanov.sh@gmail.com>
2024-05-08 10:09:38 +0300

    initial setup of core concepts done

--- sidebar-nil.js
+++ sidebar-nil.js
@@ -23,6 +23,49 @@ export default {
                 }
             ]
         },
+        {
+            type: 'category',
+            label: 'Core concepts',
+            collapsed: false,
+            collapsible: true,
+            items: [
+                {
+                    type: 'doc',
+                    label: 'Accounts',
+                    id: 'core-concepts/accounts',
+                },
+                {
+                    type: 'doc',
+                    label: 'Shards and parallelized execution',
+                    id: 'core-concepts/shards-parallel-execution',
+                },
+                {
+                    type: 'doc',
+                    label: 'Sequencing',
+                    id: 'core-concepts/sequencing',
+                },
+                {
+                    type: 'doc',
+                    label: 'Transaction lifecycle',
+                    id: 'core-concepts/transaction-lifecycle',
+                },
+                {
+                    type: 'doc',
+                    label: 'Finality',
+                    id: 'core-concepts/finality',
+                },
+                {
+                    type: 'doc',
+                    label: 'Data availability',
+                    id: 'core-concepts/data-availability',
+                },
+                {
+                    type: 'doc',
+                    label: 'Contract co-location',
+                    id: 'core-concepts/contract-co-location',
+                },
+            ]
+        },
 
     ]
 }
