5aef58b0bbf18967a77232008dd4b1190a912b31
khannanov-nil <khannanov@nil.foundation>
2024-07-04 09:22:57 +0000

    light theme disabled

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -145,7 +145,7 @@ const config = {
         },
         colorMode: {
           defaultMode: 'dark',
-          disableSwitch: false,
+          disableSwitch: true,
 
         },
         navbar: {
