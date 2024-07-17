6e0622e5b6fbe44c0a69d1bc94bb3de6709a9a49
khannanov-nil <163386217+khannanov-nil@users.noreply.github.com>
2024-07-17 14:20:16 +0300

    acknowledgment banner added (#119)
    * CLI spec update
    
    * sidebar reorg, additional info about hardhat plugin (#106)
    
    * goatcounter analytics added (#114)
    
    * Goatcounter analytics fix (#115)
    
    * goatcounter analytics added
    
    * fix to config
    
    * goatcounter test
    
    * consent banner added (#118)

--- src/theme/Root.js
+++ src/theme/Root.js
@@ -1,6 +1,39 @@
 import React from 'react';
+import CookieConsent, { Cookies, getCookieConsentValue } from "react-cookie-consent";
 
 // Default implementation, that you can customize
 export default function Root({ children }) {
-    return <>{children}</>;
+    const buttonStyle = {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: '#f2f2f2',
+        padding: '0.75rem',
+        borderRadius: '5px',
+        fontFamily: 'HelveticaNeue'
+    }
+    const contentStyle = {
+        fontFamily: 'HelveticaNeue',
+        color: '#f2f2f2'
+    }
+
+    const style = {
+        backgroundColor: '#181A1B',
+        height: '3.8em'
+    }
+    return <>
+        <CookieConsent
+            location="bottom"
+            buttonText="I understand"
+            buttonStyle={
+                buttonStyle
+            }
+            contentStyle={
+                contentStyle
+            }
+            overlay={false}
+            style={style}
+        >This website tracks page views and other usage statistics. No personal information is collected.</CookieConsent >
+        {children}
+    </>;
 }
